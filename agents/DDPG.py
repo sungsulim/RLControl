refine normalization in DDPG(input_norm, layer, batch, none), fix bug in experiment.py

agents/DDPG.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 from agents.base_agent import BaseAgent # for python3
-#from agents import Agent # for python2
+#from base_agent import BaseAgent # for python2
 
 import random
 import numpy as np
@@ -13,17 +13,18 @@ import tensorflow.contrib.slim as slim
 from agents.network.actor_network import ActorNetwork
 from agents.network.critic_network import CriticNetwork
 from utils.running_mean_std import RunningMeanStd
-from utils.rl_exp import write_summary
+from utils.main_utils import write_summary
 
 class DDPG_Network(object):
     def __init__(self, state_dim, state_min, state_max, action_dim, action_min, action_max, config, random_seed):
         
-        # type of normalization: 'none', 'batch', 'layer'
+        # type of normalization: 'none', 'batch', 'layer', 'input_norm'
         self.norm_type = config.norm
 
-        if self.norm_type == 'layer':
+        if self.norm_type == 'input_norm' or self.norm_type == 'layer' or self.norm_type == 'batch':
             self.input_norm = RunningMeanStd(state_dim)
         else:
+            assert(self.norm_type == 'none')
             self.input_norm = None
 
         self.episode_ave_max_q = 0.0
@@ -49,9 +50,6 @@ class DDPG_Network(object):
 
     def take_action(self, state, is_train):
         action = self.actor_network.predict(np.expand_dims(state, 0), False)[0]
-        # print(self.global_count, state, action)
-        # self.global_count+=1
-
         return action
 
     def update_network(self, state_batch, action_batch, next_state_batch, reward_batch, gamma_batch):
@@ -108,10 +106,8 @@ class DDPG(BaseAgent):
     def start(self, state, is_train):
         return self.take_action(state, is_train)
 
-
     def step(self, state, is_train):
         return self.take_action(state, is_train)
-        
 
     def take_action(self, state, is_train):
 
@@ -128,15 +124,16 @@ class DDPG(BaseAgent):
                 # if using an external exploration policy
                 if self.exploration_policy:
                     action = self.exploration_policy.generate(action, self.cum_steps)
+                
                 # only increment during training, not evaluation
-                self.cum_steps +=1
+                self.cum_steps += 1
 
             action = np.clip(action, self.action_min, self.action_max) 
         return action
 
-    def update(self, state, next_state, reward, action, episodeEnd):
+    def update(self, state, next_state, reward, action, is_terminal):
 
-        if not episodeEnd:
+        if not is_terminal:
             self.replay_buffer.add(state, action, reward, next_state, self.gamma)        
         else:
             self.replay_buffer.add(state, action, reward, next_state, 0.0)
@@ -153,17 +150,14 @@ class DDPG(BaseAgent):
         else:
             return
 
-
-
     # not implemented
     def get_Qfunction(self, state):
-        return None
+        raise NotImplementedError
 
     def reset(self):
         self.network.reset()
-        self.exploration_policy.reset()
+        if self.exploration_policy:
+            self.exploration_policy.reset()
 
-    # set writer
-    def set_writer(self, writer):
-        self.network.writer = writer
+
 

agents/network/actor_network.py
@@ -2,11 +2,6 @@ import tensorflow as tf
 from agents.network.base_network import BaseNetwork
 import numpy as np
 
-# used in the original DDPG paper, but not being used here
-def fanin_initializer(fanin_size):
-    value = 1.0/np.sqrt(fanin_size)
-    return tf.random_uniform_initializer(minval = -value, maxval = value)
-
 class ActorNetwork(BaseNetwork):
     def __init__(self, sess, input_norm, layer_dim, state_dim, state_min, state_max, action_dim, action_min, action_max, learning_rate, tau, norm_type):
         super(ActorNetwork, self).__init__(sess, state_dim, action_dim, learning_rate, tau)
@@ -55,7 +50,7 @@ class ActorNetwork(BaseNetwork):
                                                 if self.target_batchnorm_vars[idx].name.endswith('moving_mean:0') or self.target_batchnorm_vars[idx].name.endswith('moving_variance:0')]
 
         else:
-            assert (self.norm_type == 'none' or self.norm_type == 'layer')
+            assert (self.norm_type == 'none' or self.norm_type == 'layer' or self.norm_type == 'input_norm')
             self.batchnorm_ops = [tf.no_op()]
             self.update_target_batchnorm_params = tf.no_op()
 
@@ -71,12 +66,16 @@ class ActorNetwork(BaseNetwork):
             inputs = tf.placeholder(tf.float32, shape=(None, self.state_dim))
             phase = tf.placeholder(tf.bool)
 
+            # normalize state inputs if using "input_norm" or "layer" or "batch"
+            if self.norm_type == 'input_norm' or self.norm_type == 'layer':
+                inputs = tf.clip_by_value(self.input_norm.normalize(inputs), self.state_min, self.state_max)
+
             if self.norm_type == 'layer':
                 outputs = self.layer_norm_network(inputs, phase)
             elif self.norm_type == 'batch':
                 assert (self.input_norm is None)
                 outputs = self.batch_norm_network(inputs, phase)
-            elif self.norm_type == 'none':
+            elif self.norm_type == 'none' or self.norm_type == 'input_norm':
                 assert (self.input_norm is None)
                 outputs = self.no_norm_network(inputs, phase)
             else:
@@ -91,9 +90,8 @@ class ActorNetwork(BaseNetwork):
 
     def layer_norm_network(self, inputs, phase):
 
-        normalized_inputs = tf.clip_by_value(self.input_norm.normalize(inputs), self.state_min, self.state_max)
         # 1st fc
-        net = tf.contrib.layers.fully_connected(normalized_inputs, self.l1, activation_fn=None, \
+        net = tf.contrib.layers.fully_connected(inputs, self.l1, activation_fn=None, \
                                             weights_initializer=tf.contrib.layers.variance_scaling_initializer(factor=1.0, mode="FAN_IN", uniform=True), #tf.truncated_normal_initializer(), \
                                             weights_regularizer=None, #]tf.contrib.layers.l2_regularizer(0.001), \
                                             biases_initializer=tf.contrib.layers.variance_scaling_initializer(factor=1.0, mode="FAN_IN", uniform=True))

agents/network/critic_network.py
@@ -7,12 +7,6 @@ import matplotlib.pyplot as plt
 
 
 
-# used in the original DDPG paper, but not being used here
-def fanin_initializer(fanin_size):
-    value = 1.0/np.sqrt(fanin_size)
-    return tf.random_uniform_initializer(minval = -value, maxval = value)
-
-
 class CriticNetwork(BaseNetwork):
 
     def __init__(self, sess, input_norm, layer_dim, state_dim, state_min, state_max, action_dim, action_min, action_max, learning_rate, tau, norm_type):
@@ -83,34 +77,29 @@ class CriticNetwork(BaseNetwork):
 
             inputs = tf.placeholder(tf.float32, shape=(None,self.state_dim))
             phase = tf.placeholder(tf.bool)
-
             action = tf.placeholder(tf.float32, [None, self.action_dim])
+
+            # normalize state inputs if using "input_norm" or "layer" or "batch"
+            if self.norm_type == 'input_norm' or self.norm_type == 'layer':
+                inputs = tf.clip_by_value(self.input_norm.normalize(inputs), self.state_min, self.state_max)
 
 
             if self.norm_type == 'layer':
                 outputs = self.layer_norm_network(inputs, action, phase)
             elif self.norm_type == 'batch':
-                assert (self.input_norm is None)
                 outputs = self.batch_norm_network(inputs, action, phase)
-            elif self.norm_type == 'none':
-                assert (self.input_norm is None)
+            elif self.norm_type == 'none' or self.norm_type == 'input_norm':
                 outputs = self.no_norm_network(inputs, action, phase)
-            elif self.norm_type == 'input_norm':
-                outputs = self.input_norm_network(inputs, action, phase)
+
             else:
                 raise Exception('WRONG NORM TYPE!!')
-
-
-            
 
         return inputs, phase, action, outputs
 
 
     def layer_norm_network(self, inputs, action, phase):
-        normalized_inputs = tf.clip_by_value(self.input_norm.normalize(inputs), self.state_min, self.state_max)
-
         # 1st fc
-        net = tf.contrib.layers.fully_connected(normalized_inputs, self.l1, activation_fn=None, \
+        net = tf.contrib.layers.fully_connected(inputs, self.l1, activation_fn=None, \
                                         weights_initializer=tf.contrib.layers.variance_scaling_initializer(factor=1.0, mode="FAN_IN", uniform=True), #tf.truncated_normal_initializer(), \
                                         weights_regularizer=tf.contrib.layers.l2_regularizer(0.01), \
                                         biases_initializer=tf.contrib.layers.variance_scaling_initializer(factor=1.0, mode="FAN_IN", uniform=True))
@@ -135,7 +124,7 @@ class CriticNetwork(BaseNetwork):
 
 
     def batch_norm_network(self, inputs, action, phase):
-        # state input -> bn
+        # state input -> bn (According to paper)
         net = tf.contrib.layers.batch_norm(inputs, fused=True, center=True, scale=True, activation_fn=None,
                                             is_training=phase, scope='batchnorm_0')
         # 1st fc