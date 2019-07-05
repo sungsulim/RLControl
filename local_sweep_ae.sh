02539ef8af20911f8d4a7f76b047d68241211f1d
Sungsu Lim <ss.sungsulim@gmail.com>
2019-07-05 16:21:22 -0600

    sweep scripts for ae, ae_plus

--- local_sweep_ae.sh
+++ local_sweep_ae.sh
@@ -3,7 +3,7 @@
 ENV_NAME=Bimodal1DEnv
 AGENT_NAME=ae
 
-source /Users/sungsulim/Documents/projects/RLControl/venv/bin/activate
+source /Users/sungsulim/Documents/projects/ActorExpert/RLControl/venv/bin/activate
 #echo "Bash version ${BASH_VERSION}..."
 
 # Inclusive 
@@ -13,5 +13,5 @@ end_idx=$3
 for i in $(seq ${start_idx} ${increment} ${end_idx})
 do
    echo Running..$i
-   python3 main.py --env_json jsonfiles/environment/$ENV_NAME.json --agent_json jsonfiles/agent/$AGENT_NAME.json --index "$i" --write_plot
+   python3 main.py --env_json jsonfiles/environment/$ENV_NAME.json --agent_json jsonfiles/agent/$AGENT_NAME.json --index "$i"
 done
