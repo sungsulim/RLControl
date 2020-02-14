save progress. Fixed mean, std for forward/reverse kl in cont. bandits.

agents/network/forwardkl_network.py
@@ -163,6 +163,7 @@ class ForwardKLNetwork(BaseNetwork):
                 intgrl_q_val = self.q_net(stacked_state_batch, self.stacked_intgrl_actions)  # (8128, 1)
             tiled_intgrl_q_val = intgrl_q_val.reshape(-1, self.intgrl_actions_len)/self.entropy_scale  # (32, 254)
 
+
             # intgrl_v_val = v_val.unsqueeze(1).repeat(1, self.intgrl_actions_len, 1).reshape(-1, 1)
             # tiled_intgrl_v_val = intgrl_v_val.reshape(-1, self.intgrl_actions_len)
 
@@ -182,7 +183,7 @@ class ForwardKLNetwork(BaseNetwork):
             tiled_intgrl_logprob = intgrl_logprob.reshape(self.config.batch_size, self.intgrl_actions_len)  # (32, 254)
 
             # divide z now
-            integrands = boltzmann_prob * tiled_intgrl_logprob
+            integrands = boltzmann_prob.detach() * tiled_intgrl_logprob
             policy_loss = (-(integrands * self.tiled_intgrl_weights).sum(-1)).mean(-1)
 
         # reparam update
@@ -291,12 +292,12 @@ class PolicyNetwork(nn.Module):
 
         self.mean_linear = nn.Linear(state_dim, action_dim)
         # self.mean_linear.weight.data.uniform_(-init_w, init_w)
-        self.mean_linear.bias.data.uniform_(-1, -1)
+        self.mean_linear.bias.data.uniform_(-1.3, -0.7)
 
         self.log_std_linear = nn.Linear(state_dim, action_dim)
         self.log_std_linear.weight.data.uniform_(-init_w, init_w)
         # self.log_std_linear.bias.data.uniform_(-init_w, init_w)
-        self.log_std_linear.bias.data.uniform_(-0.5, -0.5)
+        self.log_std_linear.bias.data.uniform_(-1., -1.)
 
         self.action_dim = action_dim
         self.action_scale = action_scale

agents/network/reversekl_network.py
@@ -152,11 +152,15 @@ class ReverseKLNetwork(BaseNetwork):
 
         if self.optim_type == 'll':
 
+
             if self.use_true_q:
                 new_action, log_prob, z, mean, log_std = self.pi_net.evaluate(state_batch)
                 new_q_val = torch.from_numpy(self.predict_true_q(state_batch, new_action))
 
-            log_prob_target = new_q_val # - v_val
+                log_prob_target = new_q_val
+
+            else:
+                log_prob_target = new_q_val - v_val
 
             # loglikelihood update
             policy_loss = (-log_prob * (log_prob_target - self.entropy_scale * log_prob).detach()).mean()
@@ -290,12 +294,12 @@ class PolicyNetwork(nn.Module):
 
         self.mean_linear = nn.Linear(state_dim, action_dim)
         # self.mean_linear.weight.data.uniform_(-init_w, init_w)
-        self.mean_linear.bias.data.uniform_(-1.5, 1.5)
+        self.mean_linear.bias.data.uniform_(-1.3, -0.7)
 
         self.log_std_linear = nn.Linear(state_dim, action_dim)
-        # self.log_std_linear.weight.data.uniform_(-1, -1)
+        self.log_std_linear.weight.data.uniform_(-init_w, init_w)
         # self.log_std_linear.bias.data.uniform_(-init_w, init_w)
-        self.log_std_linear.bias.data.uniform_(-0.5, -0.5)
+        self.log_std_linear.bias.data.uniform_(-1., -1.)
 
         self.action_dim = action_dim
         self.action_scale = action_scale

plot_scripts/mergefile_new_ma.py
@@ -190,7 +190,7 @@ for idx, item in enumerate(train_mean_rewards):
 print("max train median length: ", max_median_length)
 # print(train_mean_rewards[0])
 # print(eval_mean_rewards[0])
-# print(np.shape(train_mean_rewards), np.shape(eval_mean_rewards))
+print("train shape: {}, eval shape: {}".format(np.shape(train_mean_rewards), np.shape(eval_mean_rewards)))
 # input()
 eval_mean_rewards = np.array(eval_mean_rewards)
 eval_std_rewards = np.array(eval_std_rewards)

plot_scripts/plot_agent_comparison_new.py
@@ -22,6 +22,11 @@ from plot_agent_new import get_xyrange
 
 show_labels = True
 
+display_idx = 0 # IDX to determine whether to plot train_episode or eval_episode (0: Train, 1: Eval)
+
+agents = ['ForwardKL', 'ReverseKL', 'ForwardKL_noent', 'ReverseKL_noent']
+# agents = ['ForwardKL_full_auc', 'ReverseKL_full_auc']
+
 ####### CONFIG ####
 
 # This should be the agent name between Pendulum-v0_(   )_EvalEpisode_BestResult_avg.npy
@@ -61,13 +66,17 @@ show_labels = True
 # PD
 # agents = ['ActorExpert', 'ActorExpert_LL_randomq_bimodal', 'ActorExpert_LL_randomq_unimodal']
 
-agents = ['ActorExpert_more_sampling_unimodal', 'ActorExpert_more_sampling_bimodal', 'ActorExpert_together_unimodal', 'ActorExpert_together_bimodal']
+
 
 ####################
 
 
-display_idx = 1 # IDX to determine whether to plot train_episode or eval_episode (0: Train, 1: Eval)
-
+if display_idx == 0:
+    print("==== Plotting Training ====")
+elif display_idx == 1:
+    print("==== Plotting Evaluation ====")
+else:
+    raise ValueError("Wrong display_idx")
 
 ###################
 
@@ -102,17 +111,26 @@ for ag in agents:
 
     if max_length < len(avg):
         max_length = len(avg)
+        print('agent: {}, max_length: {}'.format(ag, max_length))
 
     agents_avg.append(avg)
     agents_se.append(se)
 
 
 # mpl.style.use('default')
+# for KL paper
+colors = ['#377eb8', '#4daf4a', '#377eb8', '#4daf4a']
+# colors = ['blue', 'navy', 'green', 'teal']
 
+linestyles = ['-', '-', '--', '--']
+dashes=[(5,0), (5,0), (5,7), (5, 7)]
+
+alphas=[1.0, 1.0,1.0, 1.0]
+markers=['o', 'o', '^', '^']
 # used in thesis
-colors = [ '#377eb8', '#4daf4a', '#ff7f00',
-                  '#f781bf', '#984ea3', '#999999','#a65628',
-                  '#999999', '#e41a1c', '#dede00']
+# colors = [ '#377eb8', '#4daf4a', '#ff7f00',
+#                   '#f781bf', '#984ea3', '#999999','#a65628',
+#                   '#999999', '#e41a1c', '#dede00']
 
 # used in ss curve
 # colors = [ '#377eb8', '#4daf4a', '#ff7f00',
@@ -136,9 +154,63 @@ if xmax is None:
 # Train Episode Rewards
 if display_idx == 0:
     ylimt = (ymin[display_idx], ymax[display_idx])
-    print("Train Episode plot not implemented")
-    exit()
-    pass
+
+    if show_labels:
+        plt.title(envname)
+        plt.xlabel('Training steps (per 1000 steps)')
+        plt.ylabel("Cum. Reward per episode").set_rotation(90)
+
+    opt_range = range(0, xmax)
+    xlimt = (0, xmax-1)
+
+    print('training xmax: {}'.format(xmax))
+
+    if envname == 'Pendulum-v0':
+        # plt.axhline(y=-150, linewidth=1.0, linestyle='--', color='darkslategrey', label='-150')
+        # plt.xticks(opt_range[::20], np.linspace(0.0, float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax-1)), min(int(TOTAL_MIL_STEPS/EVAL_INTERVAL_MIL_STEPS)+1, xmax))[::20])
+        # 0, 10, 20, 30, 40, 50, 60, 70, ..., 150
+        # 9, 10, 20, 30, 40 ... 150
+
+        # 0, 1, 11, 21, 31, ... 141
+
+        loc_arr = np.array([0, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141])
+        val_arr = np.array([0.9, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0]) * 2
+
+        if not show_labels:
+            plt.xticks(loc_arr, []) #val_arr)
+            plt.yticks([-1400, -1200, -800, -400, -200, -100],[]) # [-1600, -1200, -800, -400, -200, 0])
+        else:
+            plt.xticks(loc_arr, val_arr)
+            plt.yticks([-1400, -1200, -800, -400, -200, -100],[-1400, -1200, -800, -400, -200, -100])
+
+    elif envname == 'Swimmer-v2':
+        # 1000 episodes, 991 points
+        loc_arr = np.array([0, 191, 391, 591, 791, 991])
+        val_arr = np.array([45, 200, 400, 600, 800, 1000])
+
+        if show_labels:
+            plt.xticks(loc_arr, val_arr)
+            plt.yticks([20, 25, 30, 35, 40], [20, 25, 30, 35, 40])
+        else:
+            plt.xticks(loc_arr, [])
+            plt.yticks([20, 25, 30, 35, 40], [])  # [0, 2000, 4000, 6000, 7000])
+
+    elif envname == 'Reacher-v2':
+        # 20000 episodes, 19991 points
+        loc_arr = np.array([0, 3991, 7991, 11991, 15991, 19991])
+        val_arr = np.array([45, 200, 400, 600, 800, 1000])
+
+
+        if show_labels:
+            plt.xticks(loc_arr, val_arr)
+            plt.yticks([-40, -35, -30], [-40, -35, -30])
+        else:
+            plt.xticks(loc_arr, [])
+            plt.yticks([-40, -35, -30], [])  # [0, 2000, 4000, 6000, 7000])
+    else:
+        plt.xticks(opt_range[::50], np.linspace(0.0, float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax-1)), int(TOTAL_MIL_STEPS/EVAL_INTERVAL_MIL_STEPS)+1)[::50])
+
+
 
 # EvalEpisode Rewards
 elif display_idx == 1:
@@ -165,8 +237,10 @@ elif display_idx == 1:
 
         # 0, 1, 11, 21, 31, ... 141
 
-        loc_arr = np.array([0, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141])
-        val_arr = np.array([0.9, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0])
+        # loc_arr = np.array([0, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121, 131, 141])
+        # val_arr = np.array([0.9, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0])
+        loc_arr = np.array([0, 1, 3, 5, 7, 9, 11])
+        val_arr = np.array([9, 10, 12, 14, 16, 18, 20])
 
         if not show_labels:
             plt.xticks(loc_arr, []) #val_arr)
@@ -199,22 +273,6 @@ elif display_idx == 1:
         loc_arr = np.array([0, 41, 91, 141, 191])
         val_arr = np.array([45, 250, 500, 750, 1000])
 
-        # val_arr = np.array([int(EVAL_INTERVAL_MIL_STEPS*1000*9), ])
-
-        # print(float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax+9 - 1)))
-        # print(int(TOTAL_MIL_STEPS / EVAL_INTERVAL_MIL_STEPS)+1)
-        # print(np.linspace(0, float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax+9)), int(TOTAL_MIL_STEPS / EVAL_INTERVAL_MIL_STEPS)+1)[10:20])
-        # exit()
-        # val_arr = np.append(int(EVAL_INTERVAL_MIL_STEPS*1000*9), np.linspace(float(EVAL_INTERVAL_MIL_STEPS * 1e3), float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax+9)), int(TOTAL_MIL_STEPS / EVAL_INTERVAL_MIL_STEPS))[4:20])
-        
-        # loc_arr = np.append(0, opt_range[10::tick_interval])  # np.array(range(0,xmax,tick_interval))
-        #
-        # # val_arr = np.array(range(250, 1001, 125))
-        # val_arr = np.array(range(int(EVAL_INTERVAL_MIL_STEPS*1000*9), 1001, int(EVAL_INTERVAL_MIL_STEPS*1000*tick_interval)) )
-        
-
-        # plt.xticks(np.append(1, opt_range[4::5]), np.append(2.0, np.linspace(float(EVAL_INTERVAL_MIL_STEPS * 1e3),
-        #                                                                      float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax)), int(TOTAL_MIL_STEPS / EVAL_INTERVAL_MIL_STEPS))[4::5]))
 
         if show_labels:
             plt.xticks(loc_arr, val_arr)
@@ -258,15 +316,28 @@ elif display_idx == 1:
             plt.yticks([-500, 0, 500, 1000, 1500, 2000], [])  # [0, 2000, 4000, 6000, 7000])
 
     elif envname == 'Swimmer-v2':
-        loc_arr = np.array([0, 41, 91, 141, 191])
-        val_arr = np.array([45, 250, 500, 750, 1000])
+        loc_arr = np.array([0, 16, 41, 66, 91])
+        val_arr = np.array([90, 250, 500, 750, 1000])
+
 
         if show_labels:
             plt.xticks(loc_arr, val_arr)
-            plt.yticks([20, 40, 60, 80, 100, 120], [20, 40, 60, 80, 100, 120])
+            plt.yticks([20, 25, 30, 35, 40], [20, 25, 30, 35, 40])
         else:
             plt.xticks(loc_arr, [])
-            plt.yticks([20, 40, 60, 80, 100, 120], [])  # [0, 2000, 4000, 6000, 7000])
+            plt.yticks([20, 25, 30, 35, 40], [])  # [0, 2000, 4000, 6000, 7000])
+
+    elif envname == 'Reacher-v2':
+        loc_arr = np.array([0, 16, 41, 66, 91])
+        val_arr = np.array([90, 250, 500, 750, 1000])
+
+
+        if show_labels:
+            plt.xticks(loc_arr, val_arr)
+            plt.yticks([-10, -7.5, -5, -2.5], [-10, -7.5, -5, -2.5])
+        else:
+            plt.xticks(loc_arr, [])
+            plt.yticks([-10, -7.5, -5, -2.5], [])  # [0, 2000, 4000, 6000, 7000])
 
     else:
         plt.xticks(opt_range[::50], np.linspace(0.0, float(EVAL_INTERVAL_MIL_STEPS * 1e3 * (xmax-1)), int(TOTAL_MIL_STEPS/EVAL_INTERVAL_MIL_STEPS)+1)[::50])
@@ -287,7 +358,7 @@ for idx in range(len(agents)):
                      np.append(agents_avg[idx][:xmax] - agents_se[idx][:xmax], np.zeros(pad_length) + np.nan),
                      np.append(agents_avg[idx][:xmax] + agents_se[idx][:xmax], np.zeros(pad_length) + np.nan),
                      alpha=0.2, facecolor=colors[idx])
-    handle, = plt.plot(opt_range, np.append(agents_avg[idx][:xmax], np.zeros(pad_length) + np.nan), colors[idx], linewidth=1.2, label=agents[idx])
+    handle, = plt.plot(opt_range, np.append(agents_avg[idx][:xmax], np.zeros(pad_length) + np.nan), colors[idx], linewidth=1.2, label=agents[idx], linestyle=linestyles[idx], dashes=dashes[idx])
     handle_arr.append(handle)
     
 if show_labels:

plot_scripts/plot_agent_new.py
@@ -30,20 +30,19 @@ import json
 # Use if you want to plot specific settings, put the idx of the setting below.
 # You can also see *_Params.txt to see the idx for each setting.
 
-eval_last_N = True
+eval_last_N = False
 last_N = 50
 
 selected_idx = [] # range(0 ,392, 1) # range(392,784, 1) # range(49,98,1) # #
 selected_type = selected_idx[:] # ['ae_sep bimodal', 'ae_sep unimodal', 'ae_sep bimodal w uniform', 'ae_sep unimodal w uniform'] #
 
 
-cutoff= 16
+cutoff= 8
 # ['mean, mean', 'ga, mean', 'mean, ga', 'ga, ga']#
 # ['Q-learning: x, Evaluation: x', 'Q-learning: o, Evaluation: x', 'Q-learning: x, Evaluation: o', 'Q-learning: o, Evaluation: o']
 
-
-truncate_train_ep = 2000
-# Example: selected_type = ['NAF', 'Wire_fitting']
+# truncate training ep not being used
+# truncate_train_ep = 2000
 ##############################
 
 
@@ -101,11 +100,11 @@ def get_xyrange(envname):
 
     elif envname == 'Swimmer-v2':
         ymin = [20, 20]
-        ymax = [120, 120]
+        ymax = [40, 40]
 
     elif envname == 'Reacher-v2':
-        ymin = [-50, -20]
-        ymax = [0, 0]
+        ymin = [-45, -10]
+        ymax = [-30, -3]
 
     elif envname == 'Humanoid-v2':
         ymin = [0, 0]
@@ -126,7 +125,7 @@ def get_xyrange(envname):
     elif envname == 'Pendulum-v0':
         ymin = [-1400, -1400]
         ymax = [-100, -100]
-        xmax = 72
+        # xmax = 72
 
     elif envname == 'InvertedPendulum-v2':
         ymin = [0, 0]
@@ -219,8 +218,8 @@ if __name__ == "__main__":
 
         if result == 'TrainEpisode':
             xmax = np.shape(lc)[-1]
-            if xmax > truncate_train_ep:
-                xmax = truncate_train_ep
+            # if xmax > truncate_train_ep:
+            #     xmax = truncate_train_ep
             print(xmax)
 
             plt.xlabel('Episodes')
@@ -234,6 +233,7 @@ if __name__ == "__main__":
             if xmax is None:
                 xmax = np.shape(lc)[-1] # int(max_length)
 
+            # xmax=12
             opt_range = range(0, xmax) 
             xlimt = (0, xmax-1)
 
@@ -396,6 +396,7 @@ if __name__ == "__main__":
 
             legends = [agent + ', ' + str(num_runs) + ' runs']
 
+            print(len(opt_range), len(bestlc), len(lcse))
             plt.fill_between(opt_range, bestlc - lcse, bestlc + lcse, alpha = 0.2)#, facecolor=colors[0])
             #plt.plot(opt_range, bestlc, colors[0], linewidth=1.0, label=legends)
             plt.plot(opt_range, bestlc, linewidth=1.0, label=legends)